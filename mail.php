548ba4568e0fec3b4093908d7e3c6230e71817c1
Natansz <146030104+Natansz@users.noreply.github.com>
2024-01-18 23:44:34 -0300

    confirmar

--- mail.php
+++ mail.php
@@ -14,7 +14,7 @@
         if ( empty($name) OR empty($message) OR empty($number) OR empty($subject) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo "Please complete the form and try again.";
+            echo "Preencha o formulário e tente novamente.";
             exit;
         }
 
@@ -23,7 +23,7 @@
         $recipient = "nunesnata994@gmail.com";
 
         // Set the email subject.
-        $subject = "New contact from $subject";
+        $subject = "Novo contato de $subject";
 
         // Build the email content.
         $email_content = "Name: $name\n";
@@ -38,17 +38,17 @@
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Obrigado! Sua mensagem foi enviada.";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+            echo "Ops! Algo deu errado e não foi possível enviar sua mensagem.";
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        echo "Houve um problema com seu envio. Tente novamente.";
     }
 
 ?>
